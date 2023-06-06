c3878ecda0f1db022c46ac65583c2d44ba3f6658
BirgitSollie <112555456+BirgitSollie@users.noreply.github.com>
2023-06-06 11:13:49 +0200

    Add files via upload

--- HPVcompare_Example.R
+++ HPVcompare_Example.R
@@ -8,18 +8,18 @@ library(reshape2)
 library(gtools) #for rdirichlet function
 
 #Need to load these functions once
-source('HPVcompare_functions.R')
-source('HPVcompare_RiskReduction_functions.R')
+source('Rcode/HPVcompare_functions.R')
+source('Rcode/HPVcompare_RiskReduction_functions.R')
 
 #Simulation of parameters
 nr.sim <- 1000
 vacc.up <- c(0.5,0.5)
-source('HPVcompare_ParametersData.R')
+source('Rcode/HPVcompare_ParametersData.R')
 
 #Base-case scenario (Including herd-immunity, and both AW and RRP)
 scenario <- c(2,1,3)
-source('HPVcompare_RiskReductions.R')
-source('HPVcompare_Computation.R')
+source('Rcode/HPVcompare_RiskReductions.R')
+source('Rcode/HPVcompare_Computation.R')
 
 #ICER 9v vs 2v with 95% credible interval
 round(median(Results$ICERs$'9v_vs_2v.mid'))
